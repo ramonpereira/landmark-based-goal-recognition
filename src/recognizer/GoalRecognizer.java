c7340e27bb9fd569e20036f234e0ab1addc53c46
Ramon Fraga Pereira <ramonfpereira@gmail.com>
2017-10-10 23:46:58 -0300

    Adding first release.

--- src/recognizer/GoalRecognizer.java
+++ src/recognizer/GoalRecognizer.java
@@ -10,21 +10,21 @@ public class GoalRecognizer {
 
 	public static void main(String[] args) {
 		try {
-			if (args.length < 3 || args.length > 7){
-				printUsage();
-			} else if(args.length == 3){
+			if(args.length == 3){
 				String goalRecognitionFile = args[1];
 				float threshold = Float.valueOf(args[2]);
-				if(args[0] == "filter"){
+				if(args[0].equals("-filter")){
 					GoalRecognitionFilter filter = new GoalRecognitionFilter(goalRecognitionFile, threshold);
 					filter.filter(false);
-				} else if(args[0] == "goalcompletion"){
+				} else if(args[0].equals("-goalcompletion")){
 					GoalCompletionHeuristic gc = new GoalCompletionHeuristic(goalRecognitionFile, threshold);
 					gc.recognize();
-				} else if(args[0] == "uniqueness"){
+				} else if(args[0].equals("-uniqueness")){
 					LandmarkUniquenessHeuristic uniq = new LandmarkUniquenessHeuristic(goalRecognitionFile, threshold);
 					uniq.recognize();
-				} else printUsage();
+				} else {
+					printUsage();
+				}
 			} else if(args.length == 7){
 				String domain = args[1];
 				String problem = args[2];
@@ -32,27 +32,31 @@ public class GoalRecognizer {
 				String observations = args[4];
 				String correctGoal = args[5];
 				float threshold = Float.valueOf(args[6]);
-				if(args[0] == "filter"){
+				if(args[0].equals("-filter")){
 					GoalRecognitionFilter filter = new GoalRecognitionFilter(domain, problem, goals, observations, correctGoal, threshold);
 					filter.filter(false);
-				} else if(args[0] == "goalcompletion"){
+				} else if(args[0].equals("-goalcompletion")){
 					GoalCompletionHeuristic gc = new GoalCompletionHeuristic(domain, problem, goals, observations, correctGoal, threshold);
 					gc.recognize();
-				} else if(args[0] == "uniqueness"){
+				} else if(args[0].equals("-uniqueness")){
 					LandmarkUniquenessHeuristic uniq = new LandmarkUniquenessHeuristic(domain, problem, goals, observations, correctGoal, threshold);
 					uniq.recognize();
-				} else printUsage();
-			} else printUsage();
+				} else {
+					printUsage();
+				}
+			} else {
+				printUsage();
+			}
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	private static void printUsage(){
-		System.out.println("- Option (1) - Parameters needed: <filter | goalcompletion | uniqueness> <tar.bz2 file> <threshold_value>");
-		System.out.println("\t $> Example: filter goal_recognition-problem.tar.bz2 0.1");
+		System.out.println("- Option (1) - Parameters needed: <-filter | -goalcompletion | -uniqueness> <tar.bz2 file> <threshold_value>");
+		System.out.println("\t $> Example: -filter goal_recognition-problem.tar.bz2 0.1");
 		System.out.println("\nor\n");
-		System.out.println("- Option (2) - Parameters needed: <filter | goalcompletion | uniqueness> <domain.pddl> <problem.pddl> <goals.dat> <observations.dat> <correct_goal.dat> <threshold_value>");
-		System.out.println("\t $> Example: goalcompletion domain.pddl template.pddl hyps.dat obs.dat real_hyp.dat 0.15");
+		System.out.println("- Option (2) - Parameters needed: <-filter | -goalcompletion | -uniqueness> <domain.pddl> <problem.pddl> <goals.dat> <observations.dat> <correct_goal.dat> <threshold_value>");
+		System.out.println("\t $> Example: -goalcompletion domain.pddl template.pddl hyps.dat obs.dat real_hyp.dat 0.15");
 	}
 }
